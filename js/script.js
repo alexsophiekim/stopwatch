611d365fc3eb1b3d80c03dcb6eee73a555d11467
Sophie Kim <270010565@bun2-206-17.local>
2019-05-30 11:48:08 +1200

    JS edited

--- js/script.js
+++ js/script.js
@@ -1,32 +1,52 @@
 $(document).ready(function(){
+  var timer;
+  var sec = 0;
+  var min = 0;
 
   $('#start').click(function(){
-    var sec = 0;
-    var min = 0;
-    setInterval(function(){
-      sec++;
-      if (sec === 60) {
-        min++;
-        sec =0;
-        $('#minutes').text(min);
-      }
-      $('#seconds').text(sec);
-    },1000);
-
-     $('#start').attr("disabled", true);
+    setTimeInterval();
+     $(this).attr("disabled", true);
+     $('#pause').removeAttr("disabled", true);
   });
 
   $('#pause').click(function(){
-    clearTimeout();
+    $(this).attr("disabled",true);
+    $('#start').removeAttr("disabled", true);
+    $('#reset').show();
+    clearTimeout(timer);
   });
 
   $('#continue').click(function(){
+     $(this).hide();
+     $('#reset').hide();
+     $('#pause').show();
+        setTimeInterval();
+    });
 
-  });
+    $('#reset').click(function(){
+      $(this).hide();
+      $('#start').show();
+      $('#continue').hide();
+      $('#minutes').text('0');
+      $('#seconds').text('00');
+        sec = 0 ;
+        min = 0 ;
+    })
 
-  $('#reset').click(function(){
-    setInterval();
-  });
-
+    function setTimeInterval(){
+      timer = setInterval(function(){
+        sec++;
+        if (sec === 60) {
+          min++;
+          sec =0;
+          $('#minutes').text(min);
+        }
+        if (sec < 10) {
+            $('#seconds').text('0'+sec);
+        }else {
+            $('#seconds').text(sec);
+        }
+      },1000);
+    }
 
 });
